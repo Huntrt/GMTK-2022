e5e34c943bcc76de5c61de33cf399c41102671d4
Huntrt <huntrt.business@outlook.com>
2022-07-16 09:36:57 +0700

    Adde core of an dice

---
+++ Roll To Conduct/Assets/Scripts/Dices/DiceCore.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public class DiceCore : MonoBehaviour
+{
+	[Range(1,6)] public int action;
+	[Range(1,6)] public int punish;
+
+	public int Roll()
+	{
+		return UnityEngine.Random.Range(0,6);
+	}
+}
