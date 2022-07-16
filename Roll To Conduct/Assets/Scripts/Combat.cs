8f5216571a8b3ed6e79fde70557ab8eabd28d64e
Huntrt <huntrt.business@outlook.com>
2022-07-17 01:12:29 +0700

    Complete combat queue interface

---
+++ Roll To Conduct/Assets/Scripts/Combat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Combat : MonoBehaviour
+{
+	public List<DiceCore> queues;
+	public Transform queueInterface;
+
+    //Set this class to singleton
+	public static Combat i {get{if(_i==null){_i = GameObject.FindObjectOfType<Combat>();}return _i;}} static Combat _i;
+
+	public void QueueDice(DiceCore dice)
+	{
+		//Maximum queue allow are 40
+		if(queues.Count >= 30) {print("Maxxed Dice Queue"); return;}
+		//Get the UI of queue index
+		Transform queueUI = queueInterface.GetChild(queues.Count);
+		//Add given dice to queue
+		queues.Add(dice);
+		//Update queue UI icon to match given dice icon
+		queueUI.GetChild(0).GetComponent<Image>().sprite = dice.icon;
+		//Active the queue ui
+		queueUI.gameObject.SetActive(true);
+	}
+
+	public void UnqueueDice(DiceCore dice)
+	{
+		//Go trough all the queue to check if already queue given dice
+		for (int q = 0; q < queues.Count; q++) if(queues[q] == dice)
+		{
+			//Go trough all the queue left
+			for (int u = q; u < queues.Count; u++)
+			{
+				//Get the UI of queue index
+				Transform queueUI = queueInterface.GetChild(u);
+				//Deactive the last queue UI
+				if(u == queues.Count-1) {queueUI.gameObject.SetActive(false); break;}
+				//Push the previous queue UI icon to match current dice icon
+				queueUI.GetChild(0).GetComponent<Image>().sprite = queues[u+1].icon;
+			}
+			//Remove dice out of queue
+			queues.Remove(dice);
+		}
+	}
+}
--- Roll To Conduct/Assets/Scripts/Inventory.cs
+++ Roll To Conduct/Assets/Scripts/Inventory.cs
@@ -1,15 +1,10 @@
 ﻿using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 
 public class Inventory : MonoBehaviour
 {
-
-	//%
-	public DiceType diceWanted;
-
 	public int slotAmount;
 	public List<DiceCore> dices;
 	public List<Slot> slots;
@@ -19,7 +14,6 @@ public class Inventory : MonoBehaviour
 		public DiceCore dice;
 		public Image icon;
 		public Button button;
-		public TextMeshProUGUI text;
 	}
 
 	void OnEnable()
@@ -30,11 +24,8 @@ public class Inventory : MonoBehaviour
 
 	void Update()
 	{
-		//% 
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			AddDice(diceWanted);
-		}
+		//% Testing recive weapon
+		if(Input.GetKeyDown(KeyCode.Space)) AddDice((DiceType)Random.Range(0,8));
 	}
 
 	public void AddDice(DiceType type)
@@ -55,6 +46,16 @@ public class Inventory : MonoBehaviour
 
 #region Interface
 	[SerializeField] Transform inventoryInterface;
+	public InfoPanel infoPanel; [System.Serializable] public class InfoPanel
+	{
+		public GameObject panel;
+		public TextMeshProUGUI name, description;
+	}
+
+	public SlotColor slotColor; [System.Serializable] public class SlotColor
+	{
+		public Color none, use, queued, cancel;
+	}
 
 	public void AssignSlot(int slotIndex, DiceCore dice)
 	{
@@ -62,21 +63,14 @@ public class Inventory : MonoBehaviour
 		Slot slot = slots[slotIndex];
 		//Get the Ui of slot index
 		Transform slotUI = inventoryInterface.GetChild(slotIndex);
-		//Active the slot ui
-		slotUI.gameObject.SetActive(true);
 		slot.dice = dice;
 		//@ Get the UI component
 		slot.icon = slotUI.GetChild(0).GetComponent<Image>();
 		slot.button = slotUI.GetChild(1).GetComponent<Button>();
-		slot.text = slotUI.GetChild(2).GetComponent<TextMeshProUGUI>();
 		//@ Assign UI component
 		slot.icon.sprite = dices[slotIndex].icon;
-	}
-
-	public InfoPanel infoPanel; [System.Serializable] public class InfoPanel
-	{
-		public GameObject panel;
-		public TextMeshProUGUI name, description;
+		//Active the slot ui
+		slotUI.gameObject.SetActive(true);
 	}
 
 	public void OnSlotEnter(Transform slotTF)
@@ -93,6 +87,33 @@ public class Inventory : MonoBehaviour
 		//Get the index of slot given
 		int index = slotTF.GetSiblingIndex();
 		infoPanel.panel.SetActive(false);
+	}
+
+	public void QueueingDice(Transform slotTF)
+	{
+		//Get the index of slot given
+		int index = slotTF.GetSiblingIndex();
+		//Save the combat manager
+		Combat c = Combat.i;
+		//Get the dice core of given index
+		DiceCore dice = dices[index];
+		//Get the color state of button
+		ColorBlock colors = slots[index].button.colors;
+		//If get dice are already queue
+		if(c.queues.Contains(dice)) 
+		{
+			//Remove dice from queue
+			c.UnqueueDice(dice);
+			colors.highlightedColor = slotColor.use; 
+			colors.normalColor = slotColor.none; 
+			slots[index].button.colors = colors;
+			return;
+		}
+		//Queue the dice has get
+		c.QueueDice(dice);
+		colors.normalColor = slotColor.queued; 
+		colors.highlightedColor = slotColor.cancel; 
+		slots[index].button.colors = colors;
 	}
 #endregion
 }
