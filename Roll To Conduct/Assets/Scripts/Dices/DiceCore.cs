92d06b83a0c9e212cebf4621aec3cba723ceadf2
Huntrt <huntrt.business@outlook.com>
2022-07-16 09:44:36 +0700

    Added Ui element to dice core

--- Roll To Conduct/Assets/Scripts/Dices/DiceCore.cs
+++ Roll To Conduct/Assets/Scripts/Dices/DiceCore.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
-using System;
 
 public class DiceCore : MonoBehaviour
 {
 	[Range(1,6)] public int action;
 	[Range(1,6)] public int punish;
+	[Header("UI")]
+	public Sprite icon;
+	public string description;
 
 	public int Roll()
 	{
